98911113d051b97f5e51d7bd028bd600c4a0f781
DiamondCat1234567 <240060202+DaimondCat1234567@users.noreply.github.com>
2026-03-22 19:42:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ console.log("Print. use quotes")
 var variable = "A variable"
 console.log(variable)
 console.log("Notice how I just said \"A variable\"")
+variable = "Other"
+console.log(variable)
+console.log("Don't use var to change variables!")
