b854f2aae8e6974be0fe85b5fd36dd454e597e5c
DiamondCat1234567 <240060202+DaimondCat1234567@users.noreply.github.com>
2026-03-22 19:48:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+//Prining
 console.log("Print. use quotes")
+//Variables
 var variable = "A variable"
 console.log(variable)
+// \" makes "
 console.log("Notice how I just said \"A variable\"")
 variable = "Other"
 console.log(variable)
 console.log("Don't use var to change variables!")
+// Different types of values
 var intiger = 42
 var decimalnumber = 12.3
 var boolean = True
 boolean = False
 console.log("You can use dollar in variable names!")
+/*comment!
+console.log("not outputted")
+also comment!*/
